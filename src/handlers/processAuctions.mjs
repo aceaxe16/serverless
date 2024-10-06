2e77b0822f140c3ee7434334701c67abb37410c0
aceaxe16 <branimir88@abv.bg>
2024-10-06 16:58:50 +0300

    closeAuction functionality

--- src/handlers/processAuctions.mjs
+++ src/handlers/processAuctions.mjs
@@ -1,8 +1,21 @@
+import { closeAuction } from "../libs/closeAuction.mjs";
 import { getEndedAuctions } from "../libs/getEndedAuctions.mjs";
 
+import createError from "http-errors";
+
 async function processAuctions(event, context) {
-    const auctionsToClose = await getEndedAuctions();     
-    console.log(auctionsToClose);    
+
+    try {
+        const auctionsToClose = await getEndedAuctions();
+        const closePromises = auctionsToClose.map(auction => closeAuction(auction));
+        await Promise.all(closePromises);
+
+        return { clsoed: closePromises.length }
+    } catch (err) {
+        console.log(err);
+        throw new createError.InternalServerError(err);
+
+    }
 }
 
 export const handler = processAuctions;

8721a1dd222df1d4086013d5dc00bf2b87715142
aceaxe16 <branimir88@abv.bg>
2024-10-06 12:25:30 +0300

    initial commit(saddly not working)

---
+++ src/handlers/createAuctions.mjs
@@ -0,0 +1,44 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+import { commonMiddleware } from '../libs/commonMiddleware.mjs';
+import createError from 'http-errors';
+
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+async function createAuction(event, context) {
+  const { title } = event.body;
+  const now = new Date();
+  const endDate = new Date();
+  endDate.setHours(now.getHours() + 1);
+
+  const auction = {
+    id: uuid(),
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString(),
+    // endingAt: endDate.toISOString(),
+    highestBid: {
+      amount: 0
+    },
+  }
+
+  try{
+    await dynamoDb.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction,
+    }).promise();
+  } catch(err) {
+    console.log(err);
+    throw new createError.InternalServerError(err)    
+  }
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = commonMiddleware(createAuction);
+
+
---
+++ src/handlers/getAuctions.mjs
@@ -0,0 +1,30 @@
+import AWS from 'aws-sdk';
+import { commonMiddleware } from '../libs/commonMiddleware.mjs';
+import createError from 'http-errors';
+
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuctions(event, context) {
+ let auctions;
+
+ try {
+    const result = await dynamoDb.scan({
+        TableName: process.env.AUCTIONS_TABLE_NAME
+    }).promise();
+
+    auctions = result.Items;
+ } catch(err) {
+    console.log(err);
+    throw new createError.InternalServerError(err);
+ }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auctions),
+  };
+}
+
+export const handler = commonMiddleware(getAuctions);
+
+
